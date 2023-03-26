0324e9ddf62d64049c7258c37559b4d57d4f6d98
Shaikh Noman <noman.shaikh@neosoftmail.com>
2023-03-26 19:37:36 +0530

    Page based nested routing completed

---
+++ pages/blog/first.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function First() {
+  return (
+    <div>
+      <h1>First Blog page</h1>
+    </div>
+  );
+}
+
+export default First;
---
+++ pages/blog/second.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Second() {
+  return (
+    <div>
+      <h1>Second Blog Page</h1>
+    </div>
+  );
+}
+
+export default Second;
