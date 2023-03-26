8382f83d32df6212f3fa3bb11f2f75be15b08808
Shaikh Noman <noman.shaikh@neosoftmail.com>
2023-03-27 00:04:12 +0530

    Client Link component features done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">product</Link>
     </div>
   );
 }
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,11 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
-function ProductList() {
+function ProductList({ productId = 22 }) {
   return (
     <div>
+      <Link href={"/"}>Home</Link>
       <h1>Products 1</h1>
       <h1>Products 2</h1>
-      <h1>Products 3</h1>
+      <Link href={"/products/3"} replace>
+        <h1>Products 3</h1>
+      </Link>
+      <Link href={`/products/${productId}`}>
+        <h2>Products {productId}</h2>
+      </Link>
     </div>
   );
 }
