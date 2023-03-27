f3d1bc2c6c0c0b7d168814dff38a5407023480f2
Shaikh Noman <noman.shaikh@neosoftmail.com>
2023-03-27 10:49:08 +0530

    Custom 404 page and routing using useRouter

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PageNotFOund() {
+  return (
+    <div>
+      <h1>PageNotFOund</h1>
+    </div>
+  );
+}
+
+export default PageNotFOund;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const handleClickEvent = () => {
+    console.log("cliked");
+    router.push("/products");
+  };
   return (
     <div>
       <h1>Home</h1>
       <Link href="/blog">Blog</Link>
       <Link href="/products">product</Link>
+      <button onClick={handleClickEvent}>Place Order</button>
     </div>
   );
 }
